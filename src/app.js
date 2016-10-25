c204817f749605d3379eb0045eff9e9c4c47a80f
Sarath <sarath.hacks@gmail.com>
2016-10-25 19:00:06 +0530

    Updated

--- src/app.js
+++ src/app.js
@@ -4,12 +4,18 @@ var app = new Vue({
   data:{
     message:"Hello",
     name:"Sarath",
-    rawHtml:'<p style="color:red">Html String binding using v-html</p>'
+    rawHtml:'<p style="color:red">Html String binding using v-html</p>',
+    feelBetter:true,
+    todos:['Goto store','Buy milk','Play games']
   },
   computed:{
     greetings:function(){
       return this.message +' '+ this.name;
     }
+  },
+  /*Lifecycle hooks*/
+  created:function(){
+    console.log("Vue::created");
   }
 
 })
