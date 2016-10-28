8155fcc0031ff8aa9c145d745bc5d112b00f4e44
Sarath <sarath.hacks@gmail.com>
2016-10-28 09:32:07 +0530

    Updated event handlers

--- src/event-handling.js
+++ src/event-handling.js
@@ -2,6 +2,30 @@
 var app = new Vue({
   el:'#app',
   data:{
+    name:null,
     counter:0
+  },
+  methods:{
+    setName:function(){
+      var name = prompt('Enter your name');
+      if(name){
+        this.name = name;
+      }
+    },
+    greet:function(msg,event){
+      if(this.name){
+        alert('Hi '+this.name+ ', '+msg);
+      }
+      else{
+        alert(msg);
+      }
+      if(event) event.preventDefault();
+    },
+    formSubmit:function(){
+      alert('Form submitted successfullly');
+    },
+    hitEnter:function(){
+      alert('Yuo just hitted on enter key, Awesome!!!');
+    }
   }
 });
