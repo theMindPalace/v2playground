85c2b5773b88ec195e809d6503df860ec374f078
Sarath <sarath.hacks@gmail.com>
2016-10-28 15:09:37 +0530

    Forms

---
+++ src/forms.js
@@ -0,0 +1,17 @@
+var forms = new Vue({
+  el:"#app",
+  data:{
+    name:'Sarath',
+    age:26,
+    dob:"1990-05-30",
+    bio:'',
+    country:2,
+    countryList:[{name:"US",id:1},{name:"India",id:2}],
+    subscribe:true,
+    skills:[],
+    gender:'m'
+  },
+  methods:{
+
+  }
+});
